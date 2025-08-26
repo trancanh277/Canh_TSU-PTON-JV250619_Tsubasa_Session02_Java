5eee14c9af265a0f70b757cd201d0d57b418988b
trancanh277 <tranminhcanh.tp@gmail.com>
2025-08-26 20:59:11 +0900

    upload session 2 bai tap 3

---
+++ baitap03.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class baitap03 {
+    public static void main(String[] args) {
+        int tong = 0;
+        System.out.println("Mời nhập vào số nguyên dương n");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n <= 0) {
+            System.out.println("Số nhập vào không hợp lệ");
+        } else {
+            for (int i = 1; i <= n; i++) {
+                tong += i;
+
+            }
+            System.out.printf("Tổng các số từ 1 đến %d là: %d", n, tong);
+        }
+
+    }
+}
